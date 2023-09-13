d279f34c93db19bc54c02d05ef338b05df38049d
Russ Perry <rperry1886@gmail.com>
2023-09-13 15:51:37 -0400

    Add Styling to Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <PageContent/>
-  )
+  return <Page />;
 }
 
-function PageContent() {
+function Page() {
+  return (
+    <div className="page-content">
+      <Navigation />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+function Navigation() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" className="logo" />
+        <ul className="nav__links">
+          <li className="nav__link">Pricing</li>
+          <li className="nav__link">About</li>
+          <li className="nav__link">Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
   return (
     <div>
-      <Navigation />
       <h1>Fun facts about React</h1>
       <ul className="item-list">
         <li>Was first released in 2013</li>
@@ -20,19 +42,8 @@ function PageContent() {
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-      <Footer />
     </div>
-  )
-}
-
-function Navigation() {
-  return (
-    <header>
-      <nav>
-        <img src='./react-logo.png' className='logo'/>
-      </nav>
-    </header>
-  )
+  );
 }
 
 function Footer() {
@@ -40,8 +51,7 @@ function Footer() {
     <footer>
       <small>&copy; Russ Perry. All rights reserved.</small>
     </footer>
-  )
+  );
 }
 
-
-export default App
+export default App;
