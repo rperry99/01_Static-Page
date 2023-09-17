b3eb0439d2a535098f8925e4e6fe71c80feaae90
Russ Perry <rperry1886@gmail.com>
2023-09-17 16:33:29 -0400

    Multiple Sytle Upgrades
    Added a react background element, added a name for the website, added colored bullets to the page, adjusted typography spacing

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,10 @@ function Navigation() {
   return (
     <header>
       <nav>
-        <img src="./react-logo.png" className="logo" />
+        <div className="logo-container">
+          <img src="./react-logo.png" className="logo" />
+          <p className="logo-name">React Facts</p>
+        </div>
         <p>React Course - Project 1</p>
       </nav>
     </header>
